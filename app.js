8a276fde3e42830df1d3d657ee5544002060972a
Sai Ram Gandam <ramgandam2410@gmail.com>
2026-01-06 14:14:57 +0000

    Fix CI hang by separating app and server startup

--- app.js
+++ app.js
@@ -5,9 +5,5 @@ app.get('/', (req, res) => {
   res.send('Hello DevOps CI Pipeline');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
+
